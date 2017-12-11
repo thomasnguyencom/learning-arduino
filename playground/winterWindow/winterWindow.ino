82b8257ceabb6723e90473bdafcb984fa13aa838
thomasnguyencom <contact@thomasnguyen.com>
2017-12-10 21:55:10 -0600

    Update with multiple holiday palettes

--- playground/winterWindow/winterWindow.ino
+++ playground/winterWindow/winterWindow.ino
@@ -1,18 +1,31 @@
+// ------------------------------------------------------------------------------------------------------------------------
 // https://randomnerdtutorials.com/guide-for-ws2812b-addressable-rgb-led-strip-with-arduino/
 // http://fastled.io/
-
+// ------------------------------------------------------------------------------------------------------------------------
 #include <FastLED.h>
 #include <LiquidCrystal.h>
 
 // ========================================================================================================================
-// INIT : WS2812B Multi-Color Stripdf
+// GLOBAL
+// ========================================================================================================================
+// Global variables
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+int           currentPalettePointer = 0;
+
+// ========================================================================================================================
+// INITIALIZE HARDWARE
+// ========================================================================================================================
+
+// ------------------------------------------------------------------------------------------------------------------------
+// INIT : WS2812B Multi-Color Strip
 // ------------------------------------------------------------------------------------------------------------------------
 // Wire configuration
 // -[GND] Connects to ground
-// -[DI]  (LED PIN) Add a 220 or 470 Ohm resistor between the Arduino digital output pin and the strip data input pin to reduce noise on that line.
+// -[DI]  (LED PIN) Add a 220 or 470 Ohm resistor between the Arduino digital output pin and the strip data input pin to 
+//        reduce noise on that line.
 // -[5V]  Connects to a +5V power supply
-// ========================================================================================================================
-// Configuration
+// ------------------------------------------------------------------------------------------------------------------------
 #define UPDATES_PER_SECOND 100
 #define NUM_LEDS           150
 CRGB leds[NUM_LEDS];
@@ -23,18 +36,9 @@ CRGB leds[NUM_LEDS];
 // Pin assignments
 #define FAST_LED_PIN        2
 
-// Global variables
-CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
-
-// ========================================================================================================================
+// ------------------------------------------------------------------------------------------------------------------------
 // INIT : LCD1602 LCD Screen
 // ------------------------------------------------------------------------------------------------------------------------
-// LCD display grid
-#define LCD_COLUMNS         16
-#define LCD_ROWS            2
-// ========================================================================================================================
-// Wire configuration
 // -[VSS]   Connects to ground                 Powers the LCDs
 // -[VDD]   Connects to a +5V power supply     Powers the LCDs
 // -[VO]    Connects to a (10k) Potentiometer  Adjusts the contrast of LCD1602
@@ -48,7 +52,10 @@ TBlendType    currentBlending;
 // -[D0-D7] (see below)                        Pins that read and write data
 // -[A]     Connects to a +5V power supply     Powers the LCD backlight
 // -[K]     Connects to ground                 Powers the LCD backlight
-// ========================================================================================================================
+// ------------------------------------------------------------------------------------------------------------------------
+#define LCD_COLUMNS         16
+#define LCD_ROWS            2
+
 // Pin assignments
 #define LCD_REGISTER_SELECT 7
 #define LCD_ENABLING_PIN    8
@@ -60,8 +67,12 @@ TBlendType    currentBlending;
 LiquidCrystal lcd(LCD_REGISTER_SELECT, LCD_ENABLING_PIN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 // ========================================================================================================================
-// MAIN : setup
+// MAIN
 // ========================================================================================================================
+
+// ------------------------------------------------------------------------------------------------------------------------
+// setup
+// ------------------------------------------------------------------------------------------------------------------------
 void setup()
 {
   delay( 3000 ); // power-up safety delay
@@ -69,109 +80,207 @@ void setup()
   // Setup WS2812B Strip
   FastLED.addLeds<LED_TYPE, FAST_LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
-  currentPalette = RainbowColors_p;
-  currentBlending = LINEARBLEND;
   
-
   // Setup LCD Screen
   lcd.begin(LCD_COLUMNS, LCD_ROWS);
 }
 
-// ========================================================================================================================
-// MAIN : loop
-// ========================================================================================================================
+// ------------------------------------------------------------------------------------------------------------------------
+// loop
+// ------------------------------------------------------------------------------------------------------------------------
 void loop()
 {
-    String paletteOutput = ChangePalettePeriodically();
-
-    static uint8_t startIndex = 0;
-    startIndex = startIndex + 1;    
-    FillLEDsFromPaletteColors( startIndex);
-    
-    FastLED.show();
-    FastLED.delay(1000 / UPDATES_PER_SECOND);
-
-    String secondRow = "TEST";
-    OutputLCD(paletteOutput, secondRow);
+  String paletteOutput = PalettePicker();
+  
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 1;    
+  FillLEDsFromPaletteColors( startIndex);
+  
+  FastLED.show();
+  FastLED.delay(1000 / UPDATES_PER_SECOND);
+  
+  String secondRow = "TEST";
+  OutputLCD(paletteOutput, secondRow);
 }
 
+// ========================================================================================================================
+// FUNCTIONS
+// ========================================================================================================================
+
+// ------------------------------------------------------------------------------------------------------------------------
+// FillLEDsFromPaletteColors
+// ------------------------------------------------------------------------------------------------------------------------
 void FillLEDsFromPaletteColors( uint8_t colorIndex)
 {
-    uint8_t brightness = 255;
-    
-    for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 3;
-    }
+  uint8_t brightness = 255;
+  
+  for( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+    colorIndex += 3;
+  }
 }
 
-String ChangePalettePeriodically()
+// ------------------------------------------------------------------------------------------------------------------------
+// PalettePicker
+// ------------------------------------------------------------------------------------------------------------------------
+String PalettePicker()
 {
-    uint8_t secondHand = (millis() / 1000) % 60;
-    static uint8_t lastSecond = 99;
-
-    if( lastSecond != secondHand) {
-        lastSecond = secondHand;
-
-        uint8_t paletteCounts = 2;
-        
-        uint8_t i = 0;
-        uint8_t tick = 60 / paletteCounts;
-
-        if( secondHand == (i += tick))  { SetupHolidayPalette();                    currentBlending = LINEARBLEND; }  // 1
-        if( secondHand == (i += tick))  { SetupHolidayPalette();                    currentBlending = LINEARBLEND; }  // 2
-    }
-
-    String linearDisplayName = (currentBlending == LINEARBLEND) ? "L" : "N";
-    String blendingDisplayName = GetPaletteDisplayName(currentPalette);
-    
-    //11 L Rnbw Stripe
-    return "" + String(lastSecond) + " " + linearDisplayName + " " + blendingDisplayName;
+  uint8_t paletteArrayLength = 5;
+  
+  uint8_t seconds = ((millis() / 1000)/( 1)) % 60;
+  uint8_t minutes = ((millis() / 1000)/(60)) % 60;
+  uint8_t pointer = minutes % paletteArrayLength;
+  
+  String paletteName = "";
+  
+  //if( pointer == 0)  { SetupSparkles();        currentBlending = LINEARBLEND; paletteName = "Sparkles"; }
+  
+  uint8_t i = 0;
+  if( pointer == i++) { currentBlending = LINEARBLEND; SetupGlowPalette();      paletteName = "Glow"; }
+  if( pointer == i++) { currentBlending = NOBLEND;     SetupCandyCanePalette(); paletteName = "Candy Cane"; }
+  if( pointer == i++) { currentBlending = NOBLEND;     SetupColorfulPalette();  paletteName = "Colorful"; }
+  if( pointer == i++) { currentBlending = LINEARBLEND; SetupHolidayPalette();   paletteName = "Holiday"; }
+  if( pointer == i++) { currentBlending = LINEARBLEND; SetupChasePalette();     paletteName = "Chase"; }
+  if( pointer == i++) { currentBlending = LINEARBLEND; SetupSparklesPalette();  paletteName = "Chase"; }
+  
+  return "" + String(seconds) + ":" + String(pointer) + ":" + paletteName;
 }
 
+// ------------------------------------------------------------------------------------------------------------------------
+// OutputLCD
+// ------------------------------------------------------------------------------------------------------------------------
 void OutputLCD(String firstRow, String secondRow)
 {
   lcd.clear();
   
   lcd.setCursor(0, 0);  
   lcd.print(firstRow);
-
+  
   lcd.setCursor(0, 1);  
   lcd.print(secondRow);
 }
 
-String GetPaletteDisplayName(CRGBPalette16 palette)
-{
-  String paletteName;
-       if(palette == RainbowColors_p)       { paletteName = "Rainbow1"; }
-  else if(palette == RainbowStripeColors_p) { paletteName = "Rainbow2"; }
-  else if(palette == OceanColors_p)         { paletteName = "Ocean"; }
-  else if(palette == CloudColors_p)         { paletteName = "Cloud"; }
-  else if(palette == LavaColors_p)          { paletteName = "Lava"; }
-  else if(palette == ForestColors_p)        { paletteName = "Forest"; }
-  else if(palette == PartyColors_p)         { paletteName = "Party"; }
-  else                                      { paletteName = "Custom"; }
+// ========================================================================================================================
+// PALETTE FUNCTIONS
+// ========================================================================================================================
 
-  return paletteName;
-}
-
+// ------------------------------------------------------------------------------------------------------------------------
+// SetupHolidayPalette
+// ------------------------------------------------------------------------------------------------------------------------
 void SetupHolidayPalette()
 {
-    CRGB c_red___ = CHSV( HUE_RED, 255, 255);
-    CRGB c_orange = CHSV( HUE_ORANGE, 255, 255);
-    CRGB c_yellow = CHSV( HUE_YELLOW, 255, 255);
-    CRGB c_green_ = CHSV( HUE_GREEN, 255, 255);
-    CRGB c_blue__ = CHSV( HUE_BLUE, 255, 255);
-    
-    currentPalette = CRGBPalette16(
-      c_red___, c_red___, c_red___, c_red___,
-      c_green_, c_green_, c_green_, c_green_,
-      c_red___, c_red___, c_red___, c_red___,
-      c_green_, c_green_, c_green_, c_green_
-    );
+  CRGB c_red___ = CHSV( HUE_RED, 255, 255);
+  CRGB c_green_ = CHSV( HUE_GREEN, 255, 255);
+  
+  currentPalette = CRGBPalette16(
+    c_red___, c_red___, c_red___, c_red___,
+    c_green_, c_green_, c_green_, c_green_,
+    c_red___, c_red___, c_red___, c_red___,
+    c_green_, c_green_, c_green_, c_green_
+  );
 }
 
-// ========================================================================================================================
-// REFERENCE FUNCTIONS
-// ========================================================================================================================
+// ------------------------------------------------------------------------------------------------------------------------
+// SetupCandyCanePalette
+// ------------------------------------------------------------------------------------------------------------------------
+void SetupCandyCanePalette()
+{
+  CRGB c_red___ = CRGB( 255,   0,   0);
+  CRGB c_white_ = CRGB( 255, 255, 255);
+  
+  currentPalette = CRGBPalette16(
+    c_red___, c_red___, c_red___, c_red___,
+    c_white_, c_white_, c_white_, c_white_,
+    c_red___, c_red___, c_red___, c_red___,
+    c_white_, c_white_, c_white_, c_white_
+  );
+}
 
+// ------------------------------------------------------------------------------------------------------------------------
+// SetupColorfulPalette
+// ------------------------------------------------------------------------------------------------------------------------
+void SetupColorfulPalette()
+{
+  CRGB c_red___ = CHSV( HUE_RED, 255, 255);
+  CRGB c_blue__ = CHSV( HUE_BLUE, 255, 255);
+  CRGB c_yellow = CHSV( HUE_YELLOW, 255, 255);
+  CRGB c_green_ = CHSV( HUE_GREEN, 255, 255);
+  CRGB c_orange = CHSV( HUE_ORANGE, 255, 255);
+  
+  currentPalette = CRGBPalette16(
+    c_red___, c_blue__, c_yellow, c_green_,
+    c_orange, c_red___, c_blue__, c_yellow,
+    c_green_, c_orange, c_red___, c_blue__,
+    c_yellow, c_green_, c_red___, c_blue__
+  );
+}
+
+// ------------------------------------------------------------------------------------------------------------------------
+// SetupSparklesPalette
+// ------------------------------------------------------------------------------------------------------------------------
+void SetupSparklesPalette()
+{
+  CRGB bright = CRGB(255, 255, 0);
+  CRGB medium = CRGB(253, 184, 19);
+  
+  for( int i = 0; i < 16; i++) {
+    int val = random8() % 2;
+    if(val == 0)
+    {
+      currentPalette[i] = bright;
+    }
+    else
+    {
+      currentPalette[i] = medium;
+    }
+  }
+}
+
+// ------------------------------------------------------------------------------------------------------------------------
+// SetupChasePalette
+// ------------------------------------------------------------------------------------------------------------------------
+void SetupChasePalette()
+{
+  int r = 0;
+  int g = 0;
+  int b = 0;
+  
+  int cnt = 256 / 4;
+  
+  CRGB c_0 = CRGB( r, g, b); r = r + cnt - 1; g = g + cnt - 1; b = b + cnt - 1;
+  CRGB c_1 = CRGB( r, g, b); r = r + cnt - 1; g = g + cnt - 1; b = b + cnt - 1;
+  CRGB c_2 = CRGB( r, g, b); r = r + cnt - 1; g = g + cnt - 1; b = b + cnt - 1;
+  CRGB c_3 = CRGB( r, g, b); r = r + cnt - 1; g = g + cnt - 1; b = b + cnt - 1;
+  
+  currentPalette = CRGBPalette16(
+    c_3, c_0, c_0, c_0,
+    c_0, c_0, c_0, c_0,
+    c_1, c_0, c_0, c_0,
+    c_0, c_0, c_0, c_0
+  );
+}
+
+// ------------------------------------------------------------------------------------------------------------------------
+// SetupGlowPalette
+// ------------------------------------------------------------------------------------------------------------------------
+void SetupGlowPalette()
+{
+  int r = 0;
+  int g = 0;
+  int b = 0;
+  
+  int cnt = 256 / 4;
+  
+  CRGB c_0 = CRGB( 253, 184,  19);
+  CRGB c_1 = CRGB( 255, 222,   0);
+  CRGB c_9 = CRGB( 255, 255, 255);
+  
+  currentPalette = CRGBPalette16(
+    c_1, c_0, c_1, c_0,
+    c_1, c_0, c_1, c_0,
+    c_1, c_0, c_1, c_0,
+    c_1, c_0, c_1, c_0
+  );
+}
+
+EOL
