29f9b63ee89f5d9aaec583697221f0d9716e2e42
thomasnguyencom <contact@thomasnguyen.com>
2017-12-26 16:21:54 -0600

    saveing demoReel found

---
+++ playground/demoReel/demoReel.ino
@@ -0,0 +1,148 @@
+// ------------------------------------------------------------------------------------------------------------------------
+// https://gist.github.com/kriegsman/062e10f7f07ba8518af6
+// http://fastled.io/
+// ------------------------------------------------------------------------------------------------------------------------
+#include <FastLED.h>
+
+// ========================================================================================================================
+// GLOBAL
+// ========================================================================================================================
+// Global variables
+// List of patterns to cycle through.  Each is defined as a separate function below.
+typedef void (*SimplePatternList[])();
+
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+// ========================================================================================================================
+// INITIALIZE HARDWARE
+// ========================================================================================================================
+
+// ------------------------------------------------------------------------------------------------------------------------
+// INIT : WS2811 Multi-Color Cord
+// ------------------------------------------------------------------------------------------------------------------------
+// Wire configuration
+// -[GND] Connects to ground
+// -[DI]  (LED PIN) Add a 220 or 470 Ohm resistor between the Arduino digital output pin and the strip data input pin to 
+//        reduce noise on that line.
+// -[5V]  Connects to a +5V power supply
+// ------------------------------------------------------------------------------------------------------------------------
+#define FRAMES_PER_SECOND  120
+#define NUM_LEDS           150
+#define BRIGHTNESS         96
+#define LED_TYPE           WS2811
+#define COLOR_ORDER        GRB
+CRGB leds[NUM_LEDS];
+
+// Pin assignments
+#define DATA_PIN           2
+//#define CLK_PIN          4
+
+// ========================================================================================================================
+// MAIN
+// ========================================================================================================================
+
+// ------------------------------------------------------------------------------------------------------------------------
+// setup
+// ------------------------------------------------------------------------------------------------------------------------
+void setup() {  
+  delay(3000); // 3 second delay for recovery
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+// ------------------------------------------------------------------------------------------------------------------------
+// variables
+// ------------------------------------------------------------------------------------------------------------------------
+SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+
+// ------------------------------------------------------------------------------------------------------------------------
+// loop
+// ------------------------------------------------------------------------------------------------------------------------
+void loop()
+{
+  Serial.print("Hello World");
+
+  Serial.print(gPatterns.length());
+  
+  // Call the current pattern function once, updating the 'leds' array
+  gPatterns[gCurrentPatternNumber]();
+
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+}
+
+void nextPattern()
+{
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  //fill_rainbow( leds, NUM_LEDS, gHue, 7);
+  fill_solid(leds, 50, CRGB( 255,0,0));
+}
+
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16(13,0,NUM_LEDS);
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16(i+7,0,NUM_LEDS)] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+
+}
+
+//EOL
