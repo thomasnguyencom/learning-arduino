260d80bc4246845a6d21cc80e14ebe53b10932d5
Thomas Nguyen <tnguyen@fyrsoft.com>
2018-07-09 22:29:33 -0500

    Update 5V for PIR Sensor and pins

--- playground/passiveInfrared/passiveInfrared.ino
+++ playground/passiveInfrared/passiveInfrared.ino
@@ -1,13 +1,7 @@
 // ------------------------------------------------------------------------------------------------------------------------
-// https://randomnerdtutorials.com/guide-for-ws2812b-addressable-rgb-led-strip-with-arduino/
-// http://fastled.io/
-// ------------------------------------------------------------------------------------------------------------------------
-#include <FastLED.h>
-
-// ------------------------------------------------------------------------------------------------------------------------
 // http://henrysbench.capnfatz.com/henrys-bench/arduino-sensors-and-input/arduino-hc-sr501-motion-sensor-tutorial/
 // ------------------------------------------------------------------------------------------------------------------------
-// VCC Power
+// VCC Power (5V)
 // OUT inputPin
 // GND Ground
 // ------------------------------------------------------------------------------------------------------------------------
@@ -19,7 +13,6 @@
 // GLOBAL
 // ========================================================================================================================
 // Global variables
-int ledPin = 8;                // choose the pin for the LED
 int pirState = LOW;            // we start, assuming no motion detected
 int val = 0;                   // variable for reading the pin status
 
@@ -31,7 +24,13 @@ String pirStatus = "OFF";
 // ------------------------------------------------------------------------------------------------------------------------
 // INIT : SR501 Sensor
 // ------------------------------------------------------------------------------------------------------------------------
-int inputPin = 7;               // choose the input pin (for PIR sensor)
+int pirPin = 7;               // choose the input pin (for PIR sensor)
+// ------------------------------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------------------------------
+// INIT : LED
+// ------------------------------------------------------------------------------------------------------------------------
+int ledPin = 8;                // choose the pin for the LED
 // ------------------------------------------------------------------------------------------------------------------------
 
 // ========================================================================================================================
@@ -42,8 +41,8 @@ int inputPin = 7;               // choose the input pin (for PIR sensor)
 // setup
 // ------------------------------------------------------------------------------------------------------------------------
 void setup() {
-  //pinMode(ledPin, OUTPUT);      // declare LED as output
-  pinMode(inputPin, INPUT);     // declare sensor as input
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(pirPin, INPUT);     // declare sensor as input
  
   Serial.begin(9600);
 }
@@ -52,36 +51,40 @@ void setup() {
 // loop
 // ------------------------------------------------------------------------------------------------------------------------
 void loop(){
-  val = digitalRead(inputPin);  // read input value
+  boolean showChange = false;
+  uint8_t secondHand = (millis() / 1000) % 60;
+  uint8_t minuteHand = ((millis() / 1000) / 60) % 60;
+  uint8_t hourHand = ((millis() / 1000) / 60 / 60) % 60;
+  
+// ------------------------------------------------------------------------------------------------------------------------
+  
+  val = digitalRead(pirPin);  // read input value
   
   if (val == HIGH) {            // check if the input is HIGH
     digitalWrite(ledPin, HIGH);  // turn LED ON
     if (pirState == LOW) {
-      Serial.println("Motion detected!");
       pirStatus = "ON";
-      
+
+      showChange = true;
       // We only want to print on the output change, not state
       pirState = HIGH;
-    }
-    else {
-      Serial.println("YYYYY");
     }
   } else {
     digitalWrite(ledPin, LOW); // turn LED OFF
     if (pirState == HIGH){
-      Serial.println("Motion ended!");
       pirStatus = "OFF";
       
+      showChange = true;
       // We only want to print on the output change, not state
       pirState = LOW;
     }
-    else {
-      Serial.println("ZZZZZ");
-    }
   }
-  uint8_t secondHand = (millis() / 1000) % 60;
-  uint8_t minuteHand = ((millis() / 1000) % 60) % 60;
+
+  if(showChange)
+  {
+    Serial.println("[" + String(hourHand) + " hour(s), " + String(minuteHand) + " minute(s), " + String(secondHand) + " second(s)] " + pirStatus);
+    showChange = false;
+  }
   
-  Serial.println("[" + String(minuteHand) + ":" + String(secondHand) + "] " + pirStatus);
   delay(1000);
 }
