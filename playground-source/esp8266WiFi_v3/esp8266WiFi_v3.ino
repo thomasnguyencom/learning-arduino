d8d0427b50cb6164f576f6f9fbbfb8491cf38cf1
Thomas Nguyen <tnguyen@fyrsoft.com>
2018-12-07 18:22:59 -0600

    Update esp8266WiFi_v3.ino

--- playground-source/esp8266WiFi_v3/esp8266WiFi_v3.ino
+++ playground-source/esp8266WiFi_v3/esp8266WiFi_v3.ino
@@ -1,8 +1,26 @@
-// http://www.instructables.com/id/The-First-Usage-of-ESP8266-With-Arduino-Uno/
-// https://www.itead.cc/wiki/ESP8266_Serial_WIFI_Module#AT_Commands
+// https://randomnerdtutorials.com/getting-started-with-esp8266-wifi-transceiver-review/
+// https://randomnerdtutorials.com/how-to-install-esp8266-board-arduino-ide/
 
 // ========================================================================================================================
-// NOTE: this directly interacts with the Serial Monitor, no code
+// [01] Installing the ESP8266 Board
+// ------------------------------------------------------------------------------------------------------------------------
+// 1) Open the preferences window from the Arduino IDE.
+//         Go to File > Preferences
+// 2) Enter http://arduino.esp8266.com/stable/package_esp8266com_index.json
+//         into the “Additional Board Manager URLs” field as shown in the figure below. 
+//         Then, click the “OK” button.
+// 3) Open boards manager.
+//         Go to Tools > Board > Boards Manager…
+//         Arduino IDE boards manager
+// 4) Scroll down, select the ESP8266 board menu and install “esp8266”
+// 5) Choose your ESP8266 board from Tools > Board > Generic ESP8266 Module
+// 6) Finally, re-open your Arduino IDE
+// ========================================================================================================================
+
+// ========================================================================================================================
+// [02] Testing the Installation
+// ------------------------------------------------------------------------------------------------------------------------
+
 // ========================================================================================================================
 
 // ========================================================================================================================
@@ -43,54 +61,6 @@
 // Set Both NL & CR
 // Set baud rate 115200 (default)
 // ------------------------------------------------------------------------------------------------------------------------
-
-// ------------------------------------------------------------------------------------------------------------------------
-// AT Command Format: 
-// ------------------------------------------------------------------------------------------------------------------------
-// SET  AT+<cmd>=<…>
-// GET  AT+<cmd>?
-// TEST AT+<cmd>=?
-// EXEC AT+<cmd>
-
-// ------------------------------------------------------------------------------------------------------------------------
-// Basic Commands             Verb Behavior                      Notes
-// ------------------------------------------------------------------------------------------------------------------------
-// AT                         EXEC Check module status      
-// AT+CIOBAUD=9600            SET  Update baud rate              Default: 115200
-// AT+RST                     -    Reset the module
-// AT+GMR                     EXEC Firmware version              AT version:1.2.0.0(Jul  1 2016 20:04:45)
-//                                                               SDK version:1.5.4.1(39cb9a32)
-//                                                               Ai-Thinker Technology Co. Ltd.
-//                                                               Dec  2 2016 14:21:16
-
-// ------------------------------------------------------------------------------------------------------------------------
-// WiFi Commands              Verb Behavior                      Notes
-// ------------------------------------------------------------------------------------------------------------------------
-// AT+CWMODE?                 GET  Get network mode              1 = Sta
-//                                                               2 = AP (AP is a normal mode for devices)
-//                                                               3 = both (default mode of router)
-// AT+CWLAP                   EXEC List WiFi
-// AT+CWJAP?                  GET  Get connected WiFi
-// AT+CWJAP                   EXEC Connect to the AP             AT+ CWJAP =<ssid>,<pwd>
-// AT+CWQAP                   EXEC Disconnect to the AP
-// AT+CWSAP                   EXEC Set params of AP              AT+ CWSAP= <ssid>,<pwd>,<chl>, <ecn>
-// AT+CWLIF                   EXEC Check join device's IP
-
-// ------------------------------------------------------------------------------------------------------------------------
-// TCP/IP Commands            Verb Behavior                      Notes
-// ------------------------------------------------------------------------------------------------------------------------
-// AT+CIPSTATUS               EXEC                               <id>,<type>,<addr>,<port>,<tetype>= client or server mode
-//                                                               status:
-//                                                                 2: Got IP
-//                                                                 3: Connected
-//                                                                 4: Disconnected
-//                                                               id： id of the connection (0~4), for multi-connect
-//                                                               type： string, “TCP” or “UDP”
-//                                                               addr： string, IP address.
-//                                                               port： port number
-//                                                               tetype：
-//                                                                 0: ESP8266 runs as client
-//                                                                 1: ESP8266 runs as server
 
 void setup()
 {
