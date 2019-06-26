4d494045eccddafcdfdce52ab84fe69e622c81da
Thomas Nguyen <tnguyen@fyrsoft.com>
2019-06-26 17:51:52 -0500

    Add dynamic html page for ON/OFF

--- playground-source/nodemcuwebserver/nodemcuwebserver.ino
+++ playground-source/nodemcuwebserver/nodemcuwebserver.ino
@@ -4,20 +4,21 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
-// Replace with your network credentials
+// Network contants
 const char* ssid = "gorideabike";
 const char* password = "tomisthebomb";
+const String LED_STATE_KEY = "#LED_STATE_KEY#";
 
-ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
+//instantiate server at port 80 (http port)
+ESP8266WebServer server(80);
 
-String page = "";
-int LEDPin = 13; //12
+// LED pin definitions
+int ledPin = 13;
+
 void setup(void){
-  //the HTML of the web page
-  page = "<h1>Simple NodeMCU Web Server</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p>";
   //make the LED pin output and initially turned off
-  pinMode(LEDPin, OUTPUT);
-  digitalWrite(LEDPin, LOW);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
    
   delay(1000);
   Serial.begin(115200);
@@ -36,16 +37,16 @@ void setup(void){
   Serial.println(WiFi.localIP());
    
   server.on("/", [](){
-    server.send(200, "text/html", page);
+    server.send(200, "text/html", GetHtmlPage(""));
   });
   server.on("/LEDOn", [](){
-    server.send(200, "text/html", page);
-    digitalWrite(LEDPin, HIGH);
+    server.send(200, "text/html", GetHtmlPage("ON"));
+    digitalWrite(ledPin, HIGH);
     delay(1000);
   });
   server.on("/LEDOff", [](){
-    server.send(200, "text/html", page);
-    digitalWrite(LEDPin, LOW);
+    server.send(200, "text/html", GetHtmlPage("OFF"));
+    digitalWrite(ledPin, LOW);
     delay(1000); 
   });
   server.begin();
@@ -54,4 +55,14 @@ void setup(void){
  
 void loop(void){
   server.handleClient();
+}
+
+String GetHtmlPage(String ledState){
+  String pageTemplate = "<html><body><h1>Simple NodeMCU Web Server</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p><p>#LED_STATE_KEY#</p></body></html>";
+
+  pageTemplate.replace(LED_STATE_KEY, ledState);
+  String page = pageTemplate;
+  
+  return page;
+
 }
